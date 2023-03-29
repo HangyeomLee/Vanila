8f1f79679df73e3b90ff59b2d6b30d747dabfd65
Hangyeom Lee <h38lee@uwaterloo.ca>
2023-03-29 17:47:22 -0400

    Error fixed and saved todos in local storage

--- js/todo.js
+++ js/todo.js
@@ -1,13 +1,27 @@
 const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input")
-const toDoList = document.querySelector("todo-list");
+const toDoList = document.querySelector("#todo-list");
+
+const toDos=[];
+
+function saveToDos(){
+    localStorage.setItem("todos",JSON.stringify(toDos));
+}
+
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
 
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
     const button = document.createElement("button");
     li.appendChild(span);
+    li.appendChild(button);
     span.innerText = newTodo;
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo);
     toDoList.appendChild(li);
 }
 
@@ -15,6 +29,7 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
 }
 
