06291494eaeee40c1f5f128f9cc59842dc68081e
Hangyeom Lee <h38lee@uwaterloo.ca>
2023-04-05 13:25:08 -0400

    pm am updates

--- js/clock.js
+++ js/clock.js
@@ -4,15 +4,19 @@ let CLOCK_TEXT = null;
 function getClock(){
     const date = new Date();
     let hours = String(date.getHours()).padStart(2,"0");
-    if(hours >12){
+    let pmam = true;
+    if(hours > 12){
         hours = hours - 12;
-        hours = hours + " PM";
-    }
-    else{
-        hours = hours + " AM";
+        pmam = false
     }
     let minutes = String(date.getMinutes()).padStart(2,"0");
     let seconds = String(date.getSeconds()).padStart(2,"0");
+    if(pmam === false){
+        seconds = seconds +" PM";
+    }
+    else{
+        seconds = seconds +" AM";
+    }
     CLOCK_TEXT = `${hours}:${minutes}:${seconds}`;
     clock.innerText = CLOCK_TEXT;
 }
